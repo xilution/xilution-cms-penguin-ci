6ea29b6a1e759a7928e04b247a969be1d44adfa4
Todd Brunia <tbrunia@gmail.com>
2020-02-26 07:05:06 -0600

    chore: moved databse to giraffe

--- variables.tf
+++ variables.tf
@@ -13,34 +13,3 @@ variable "pipeline_id" {
   type = string
   description = "The Pipeline ID"
 }
-
-variable "client_aws_account" {
-  type = string
-  description = "The Xilution Client AWS Account ID"
-}
-
-variable "k8s_cluster_name" {
-  type = string
-  description = "The Kubernetes Cluster Name"
-  default = "xilution-k8s"
-}
-
-variable "master_username" {
-  type = string
-  description = "The Database Username"
-}
-
-variable "master_password" {
-  type = string
-  description = "The Database Password"
-}
-
-variable "docker_username" {
-  type = string
-  description = "The Docker Hub username"
-}
-
-variable "docker_password" {
-  type = string
-  description = "The Docker Hub password"
-}
