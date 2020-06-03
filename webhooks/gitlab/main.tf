7d29c30905782afd5bd7a2dea621530f1735756a
Spenser Andreassen <spenserca@gmail.com>
2020-06-02 20:22:16 -0500

    chore: stubbing basic gitlab webhook config

--- webhooks/gitlab/main.tf
+++ webhooks/gitlab/main.tf
@@ -1,9 +1,10 @@
 provider "gitlab" {
-  token = "${var.gitlab_token}"
+  token = "${var.gitlab_token}" // auth token
 }
 
-# Add a hook to the project
-resource "gitlab_project_hook" "sample_project_hook" {
-  project = "${gitlab_project.sample_project.id}"
-  url = "https://example.com/project_hook"
+resource "gitlab_project_hook" "xilution-pipeline-hook" {
+  project = "gitlab_project/gitlab_repo" // "spenserca/spenser-wordpress-docker"
+  url = "https://webhooks.xilution.com/gitlab/repository/push"
+  push_events = true
+  token = "penguin-pipeline-id" // X-Gitlab-Token header value
 }
