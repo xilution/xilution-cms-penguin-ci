bc1352ab0053550e481bd462ddd2679649e54d2f
Todd Brunia <tbrunia@gmail.com>
2020-11-11 20:07:06 -0600

    feat: fixed workpress release name

--- scripts/deploy-down.sh
+++ scripts/deploy-down.sh
@@ -2,6 +2,7 @@
 
 pipelineId=${PENGUIN_PIPELINE_ID}
 stageName=${STAGE_NAME}
+stageNameLower=$(echo "${stageName}" | tr '[:upper:]' '[:lower:]')
 
-helm tiller run tiller -- helm delete wordpress-"$pipelineId"-"$stageName"
-helm tiller run tiller -- helm del --purge wordpress-"$pipelineId"-"$stageName"
+helm tiller run tiller -- helm delete wordpress-"${pipelineId:0:8}"-"${stageNameLower}"
+helm tiller run tiller -- helm del --purge wordpress-"${pipelineId:0:8}"-"${stageNameLower}"
--- scripts/deploy-up.sh
+++ scripts/deploy-up.sh
@@ -3,17 +3,15 @@
 buildDir=${CODEBUILD_SRC_DIR_BuildDeployImage}
 pipelineId=${PENGUIN_PIPELINE_ID}
 stageName=${STAGE_NAME}
+stageNameLower=$(echo "${stageName}" | tr '[:upper:]' '[:lower:]')
 loadBalancerHostname=$(kubectl get services/ingress-nginx -n ingress-nginx -o json | jq -r '.status.loadBalancer.ingress[0].hostname')
-
-echo "the contents of the build dir is:"
-ls "${buildDir}"
 
 helm tiller run tiller -- helm upgrade \
   --install \
   --force \
   --namespace wordpress \
-  --set mysql.name=wordpress-"$pipelineId"-"$stageName" \
+  --set mysql.name=wordpress-"${pipelineId:0:8}"-"${stageNameLower}" \
   --set host="$loadBalancerHostname" \
-  --set path=/wordpress/"$pipelineId"/"$stageName" \
-  --values "$buildDir"/image.yaml \
-  wordpress-"$pipelineId"-"$stageName" ./helm/wordpress
+  --set path=/wordpress/"${pipelineId:0:8}"/"${stageNameLower}" \
+  --values "${buildDir}"/image.yaml \
+  wordpress-"${pipelineId:0:8}"-"${stageNameLower}" ./helm/wordpress
